d34ad8b2cebd4d52ab2555a7d5c9b134225e77e6
Yiyun Liu <liuyiyun@terpmail.umd.edu>
2020-03-02 18:13:47 -0500

    split up foldable

--- src/Main.hs
+++ src/Main.hs
@@ -19,28 +19,35 @@ removeTmpAndRunLiquid str =
 
 functors :: [FilePath]
 functors =
-  [ "Data/Either/Functor.hs"
-  , "Data/Functor/Const/Functor.hs"
-  , "Data/Functor/Identity/Functor.hs"
-  , "Data/Functor/State/Functor.hs"
-  , "Data/List/Functor.hs"
-  , "Data/Maybe/Functor.hs"
-  , "Data/Reader/Functor.hs"
+  [ "Data/Either/Functor.hs" -- VMonad
+  , "Data/Functor/Const/Functor.hs" -- VFunctor
+  , "Data/Functor/Identity/Functor.hs" -- VMonad
+  , "Data/Functor/State/Functor.hs" -- VFunctor
+  , "Data/List/Functor.hs" -- VMonad
+  , "Data/Maybe/Functor.hs" -- VMonad
+  , "Data/Reader/Functor.hs" -- VApplicative
   ]
 
 semigroups :: [FilePath]
 semigroups =
-  [ "Data/All/Semigroup.hs"
-  , "Data/Any/Semigroup.hs"
-  , "Data/Dual/Semigroup.hs"
-  , "Data/Endo/Semigroup.hs"
-  , "Data/Functor/Identity/Semigroup.hs"
-  , "Data/List/Semigroup.hs"
-  , "Data/Maybe/Semigroup.hs"
-  , "Data/Num/Semigroup.hs"
-  , "Data/PNat/Semigroup.hs"
+  [ "Data/All/Semigroup.hs" -- VMonoid
+  , "Data/Any/Semigroup.hs" -- VMonoid
+  , "Data/Dual/Semigroup.hs" -- VMonoid 
+  , "Data/Endo/Semigroup.hs" -- VMonoid
+  , "Data/Functor/Identity/Semigroup.hs" -- VMonoid
+  , "Data/List/Semigroup.hs" -- VMonoid
+  , "Data/Maybe/Semigroup.hs" -- VMonoid
+  , "Data/Num/Semigroup.hs" -- VSemigroup
+  , "Data/PNat/Semigroup.hs" -- VMonoid
   ]
 
+foldables :: [FilePath]
+foldables =
+  [ "Data/Functor/Const/Foldable.hs"
+  , "Data/Functor/Identity/Foldable.hs"
+  , "Data/List/Foldable.hs"
+  , "Data/Maybe/Foldable.hs"
+  ]
 
 makeBench :: FilePath -> Benchmark
 makeBench f = bench f $ nfIO (removeTmpAndRunLiquid f)
@@ -51,8 +58,8 @@ main = defaultMainWith
   [ bgroup "Semigroup" $ fmap makeBench semigroups
   , bgroup "Functor" $ fmap makeBench functors
   -- very very slow!
-  , bgroup "Succs" [makeBench "Data/Successors/Functor.hs"]
-  , bgroup "Foldable" [makeBench "Data/Foldable/Classes.hs"]
+  -- , bgroup "Succs"    [makeBench "Data/Successors/Functor.hs"]
+  , bgroup "Foldable" $ fmap makeBench foldables
   ]
 
 
