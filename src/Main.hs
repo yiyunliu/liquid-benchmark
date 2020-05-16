54ec5a5181c474018fcde397c0f4584075b8d3db
James Parker <dev@jamesparker.me>
2020-05-16 00:11:02 -0400

    all vrdt benchmarks except 2pmap

--- src/Main.hs
+++ src/Main.hs
@@ -4,19 +4,48 @@ import           Criterion.Main
 import           Language.Haskell.Liquid.Liquid
 import           System.Exit
 import           System.Directory
+import           Control.DeepSeq
 import           Control.Monad
 import           Control.Exception
+-- import           Criterion.Measurement (time_)
 import           Criterion.Types
+import qualified Data.Map as Map
+import           Numeric.Statistics.Moment
+import           System.Process
+import           System.TimeIt
 
-removeTmpAndRunLiquid :: String -> IO ()
-removeTmpAndRunLiquid str =
+benchmark :: Int -> [String] -> IO (Map.Map String [Double])
+benchmark n files = helper mempty n files
+
+  where
+    helper acc c _ | c < 0 = return acc
+    helper acc c []        = do
+        -- delete temp
+        callCommand $ "find " <> dir <> " | grep \".liquid\" | xargs rm -rf"
+        
+        helper acc (c-1) files
+    helper acc c (f:fs)    = do
+      t <- fmap fst $ timeItT $ do
+        res <- removeTmpAndRunLiquid dir f
+        return $ force res
+
+      helper (Map.insertWith (++) f [t] acc) c fs
+
+    -- WARNING: Be careful with this string! It is sent to a shell command.
+    dir = "vrdt/vrdt/src/"
+
+
+removeTmpAndRunLiquid :: String -> String -> IO ()
+removeTmpAndRunLiquid dir str =
   handle handler
-    . withCurrentDirectory "liquid-base/liquid-base/src"
-    $ liquid [str]
+    -- . withCurrentDirectory dir -- "liquid-base/liquid-base/src"
+    $ liquid $ ["-i", dir] <> args <> [dir <> str]
 
   where
     handler ExitSuccess = pure ()
     handler e           = throw e
+
+    args = words "--typeclass --ghc-option=-XTypeFamilies --ghc-option=-XFlexibleContexts --ghc-option=-XBangPatterns --ghc-option=-XTypeFamilyDependencies --ghc-option=-cpp"
 
 
 dependencies :: [FilePath]
@@ -73,17 +102,58 @@ foldables =
   ]
 
 makeBench :: FilePath -> Benchmark
-makeBench f = bench f $ nfIO (removeTmpAndRunLiquid f)
+makeBench f = bench f $ nfIO (removeTmpAndRunLiquid "liquid-base/liquid-base/src" f)
+
+vrdt = "VRDT/Class.hs"
+strongconvergence = "VRDT/Class/Proof.hs"
+multiset = "VRDT/MultiSet.hs" -- , "vrdt/vrdt/src/VRDT/MultiSet/Internal.hs"]
+
+
+vrdtModules :: [FilePath]
+vrdtModules = [
+  -- Dependencies
+    "Liquid/Data/Maybe.hs"
+  , "Liquid/ProofCombinators.hs"
+  , "Liquid/Data/Map.hs"
+  , "Liquid/Data/Map/Props.hs"
+  , "Liquid/Data/List.hs"
+  , "VRDT/Class.hs"
+
+  -- Benchmarks
+  , "VRDT/Max.hs"
+  , "VRDT/Min.hs"
+  , "VRDT/Sum.hs"
+  , "VRDT/LWW.hs"
+  , "VRDT/MultiSet.hs"
+  , "VRDT/Class/Proof.hs"
+  -- , "VRDT/TwoPMap.hs"
+  ]
+
 
 main :: IO ()
-main = defaultMainWith
-  defaultConfig
-  [ bgroup "Dependencies" $ fmap makeBench dependencies
-  , bgroup "Semigroup" $ fmap makeBench semigroups
-  , bgroup "Functor" $ fmap makeBench functors
-  -- very very slow!
-  , bgroup "Foldable" $ fmap makeBench foldables
-  , bgroup "Succs"    [makeBench "Data/Successors/Functor.hs"]
-  ]
+-- main = defaultMainWith
+--   defaultConfig
+--   [ bgroup "Dependencies" $ fmap makeBench dependencies
+--   , bgroup "Semigroup" $ fmap makeBench semigroups
+--   , bgroup "Functor" $ fmap makeBench functors
+--   -- very very slow!
+--   , bgroup "Foldable" $ fmap makeBench foldables
+--   , bgroup "Succs"    [makeBench "Data/Successors/Functor.hs"]
+--   ]
+main = do
+
+  -- Map FilePath [Time]
+  res <- benchmark n vrdtModules
+  -- let res = Map.fromList [("test", [1,2,3])]
+  mapM_ (\(filepath, times) -> do
+      putStrLn filepath
+      putStrLn $ show times
+      putStrLn $ show (mean times) <> " (" <> show (stddev times) <> ")"
+      putStrLn ""
+    
+    ) $ Map.toList res
+
+  where
+    n = 3
 
 
